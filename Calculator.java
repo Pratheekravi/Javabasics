f7426b051821d8ba559e0069acf16ad13c8e043a
Pratheekravi <pratheekr45@gmail.com>
2024-03-27 19:36:39 +0530

    added the functionality for Calculator by using variables

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    public static void main (String [] args){
+        int x = 10;
+        int y = 5;
+        //int z = (x + y);
+        System.out.println("the result of 2 numbers are " +(x + y));
+
+        //z = (x - y);
+        System.out.println("the result of 2 numbers are " +(x - y));
+
+        //z = (x * y);
+        System.out.println("the result of 2 numbers are " +(x * y));
+
+        //z = (x/y);
+        System.out.println("the result of 2 numbers are " +(x/y));
+
+
+
+
+
+
+
+    }
+}
+
