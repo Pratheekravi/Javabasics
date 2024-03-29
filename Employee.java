4a5e1a4ad938252f70c4831d6191280d314dded4
Pratheekravi <pratheekr45@gmail.com>
2024-03-29 08:29:18 +0530

    added class Employee

---
+++ Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+    public static void employeeDetails() {
+        String Nameoftheperson = "Abhishek";
+        int age = 32;
+        int salary = 60000;
+        String Department = "Publichealth";
+    }
+
+    public static void main (String[] abc){
+        String Abhishek = "Abhishek";
+        System.out.println("Nameoftheperson " +Abhishek);
+        System.out.println("age " +32);
+        System.out.println("salary " +60000);
+        String Publichealth = "Publichealth";
+        System.out.println("Department " +Publichealth);
+
+    }
+    }
+
+
